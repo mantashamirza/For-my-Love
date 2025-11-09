00e8f19f869f7edd1df2873dc7fdf4c400f694a0
mantashamirza <mantashamirza333@gmail.com>
2025-11-09 17:59:25 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// Sparkle generator
+function createSparkle() {
+    const sparkle = document.createElement("div");
+    sparkle.classList.add("sparkle");
+
+    sparkle.style.left = Math.random() * window.innerWidth + "px";
+    sparkle.style.top = window.innerHeight + "px";
+    sparkle.style.animationDuration = (4 + Math.random() * 3) + "s";
+
+    document.querySelector(".sparkle-container").appendChild(sparkle);
+
+    setTimeout(() => sparkle.remove(), 7000);
+}
+
+setInterval(createSparkle, 300);
+
+// Open page function
+function openSurprise() {
+    window.location.href = "main.html";
+}
